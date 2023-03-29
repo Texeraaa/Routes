00a1c51979b0a2d0cac4cac1db6e26e44973d336
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2023-03-29 14:48:02 -0300

    Update 1.0

---
+++ src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="usuario-detalhe">
+        <h3>Usuario Detalhe</h3>
+        <hr>
+        <p><strong>Código:</strong> {{  id  }}</p>
+        <router-link tag="button" primario 
+            :to="{ name: 'editarUsuario', params: { id },
+                query: { completo : true, lingua: 'pt-br'},
+                hash: '#rodape' }">
+            Editar
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    beforeRouteEnter(to, from, next){
+        console.log('dentro do componente -> usuario detalhe')
+        next()
+    }
+    // data(){
+    //     return{
+    //         id: this.$route.params.id
+    //     }
+    // },
+    // watch: {
+    //     $route(to,from){
+    //         this.id = to.params.id
+    //     }
+    // },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="usuario-editar">
+        <h3>Usuario Editar</h3>
+        <hr>
+        <p><strong>Código: </strong>{{ id }} | {{ $route.params.id }}</p>
+        <p><strong>Completo: </strong>{{ $route.query.completo ? 'Sim' : 'Não' }}</p>
+        <p><strong>Lingua: </strong>{{ $route.query.lingua }}</p>
+        <hr>
+        <button primario @click="confirmou = true">Confirmar</button>
+        <div id="rodape">
+            <h3>Curso Vue</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    data(){
+        return{
+            confirmou: false
+        }
+    },
+    beforeRouteLeave(to, from, next){
+        if(this.confirmou){
+            next()
+        } else {
+            if (confirm('Tem certeza?')){
+                next(false)
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    #rodape{
+        margin-top: 500px;
+    }
+</style>
